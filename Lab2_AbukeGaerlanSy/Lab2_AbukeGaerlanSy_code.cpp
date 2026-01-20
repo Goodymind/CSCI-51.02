9f37a71a9b19bc5b0de0efb7812130dfb64c7606
Goodymind <aabuke9@gmail.com>
2026-01-20 23:23:12 +0800

    comments and test values

--- Lab2_AbukeGaerlanSy/Lab2_AbukeGaerlanSy_code.cpp
+++ Lab2_AbukeGaerlanSy/Lab2_AbukeGaerlanSy_code.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int parseMessage(string msgp)
 {
-    for (int i = 0; i < msgp.length(); i++)
+    for (int i = 0; i < msgp.length(); i++)             // iterates through every character.
     {
-        if (!isdigit(msgp[i]))
+        if (!isdigit(msgp[i]))                          // checks if the character is a digit
         {
-            return 0;
+            return 0;                                   // returns 0 immediately if nondigit detected.
         }
     }
 
@@ -18,26 +18,26 @@ int parseMessage(string msgp)
 
 int main(void)
 {
-    int n, x, y;
-    string message;
-    cin >> n;
-    for (int i = 1; i <= n; i++)
+    int n, x, y;                                        // Initialize Variables
+    string message;                                     // --
+    cin >> n;                                           // get n;
+    for (int i = 1; i <= n; i++)                        // for loop that iterates from Agent #1 to Agent #n
     {
 
-        cin >> x >> y;
-        getline(cin >> ws, message);
+        cin >> x >> y;                                  // get x, y
+        getline(cin >> ws, message);                    // discard the space after y (ws), save message string as whole
 
-        cout << "Agent #" << i << " is at (" << x << "," << y << ")" << endl;
+        cout << "Agent #" << i << " is at (" << x << "," << y << ")" << endl;   // output coordinates of Agent #i
         
-        int m = parseMessage(message);
+        int m = parseMessage(message);                                          // custom function;
         
-        if (m == 0) 
+        if (m == 0)                                                             // if nondigit message
         {
-            cout << "Agent #" << i << " yells: \"" << message << "\"" << endl;
+            cout << "Agent #" << i << " yells: \"" << message << "\"" << endl;  // output  message
         }
         else
         {
-            cout << "Agent #" << i << " holds up the number: " << m << endl;
+            cout << "Agent #" << i << " holds up the number: " << m << endl;    // output the number
         }
     }
     return 0;
