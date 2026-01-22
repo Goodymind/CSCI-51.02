f48c41140d635e524174f8b3359af491bfa2fb4a
Goodymind <aabuke9@gmail.com>
2026-01-22 18:09:46 +0800

    rstrip comments

--- Lab2_AbukeGaerlanSy/Lab2_AbukeGaerlanSy_code.cpp
+++ Lab2_AbukeGaerlanSy/Lab2_AbukeGaerlanSy_code.cpp
@@ -5,20 +5,20 @@ using namespace std;
 
 void rstrip(string* s) 
 {
-    int trimsize = 0;
-    for (int i = s->length() - 1; i >= 0; i--)
+    int trimsize = 0;                               // initialize number of whitespaces to remove to 0l
+    for (int i = s->length() - 1; i >= 0; i--)      // iterate through every character from the right.
     {
         if ((*s)[i] == 32 || (*s)[i] == 9 || (*s)[i] == 10 || (*s)[i] == 11 || (*s)[i] == 12 || (*s)[i] == 13) 
-        {
+        { // if character is a whitespace (defined by ASCII), add 1 to trimsize;
             trimsize++;
         }
         else 
         {
-            break;
+            break;  // break loop if non whitespace detected.
         }
     }
-    s->resize(s->length() - trimsize);
-    s->shrink_to_fit();
+    s->resize(s->length() - trimsize);      // string function that resizes a string given length. This removes the whitespaces
+    s->shrink_to_fit();                     // reduces the string's capacity to its new reduced size.
 }
 
 int parseMessage(string msgp)
@@ -43,7 +43,7 @@ int main(void)
     {
 
         cin >> x >> y;                                  // get x, y
-        getline(cin >> ws, message);                    // discard the space after y (ws), save message string as whole
+        getline(cin >> ws, message);                    // discard the whitespace after y (ws), save message string as whole
 
         cout << "Agent #" << i << " is at (" << x << "," << y << ")" << endl;   // output coordinates of Agent #i
         
@@ -51,7 +51,7 @@ int main(void)
         
         if (m == 0)                                                             // if nondigit message
         {
-            rstrip(&message);
+            rstrip(&message);                                                   // custom function that removes trailing whitespaces 
             cout << "Agent #" << i << " yells: \"" << message << "\"" << endl;  // output  message
         }
         else
