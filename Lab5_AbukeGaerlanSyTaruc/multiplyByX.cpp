25303aaeead252a5ab806163fd5a05aa1a76be66
Goodymind <aabuke9@gmail.com>
2026-02-20 03:05:23 +0800

    Step 2

--- Lab5_AbukeGaerlanSyTaruc/multiplyByX.cpp
+++ Lab5_AbukeGaerlanSyTaruc/multiplyByX.cpp
@@ -2,6 +2,9 @@
  
 void multiplyByX(IntArray *p, int x) 
 { 
-    // multiply x with all elements of IntArray 
-    // (overwrite the previous elements) 
+    for (int i = 0; i < p->size; i++)
+    {
+        p->elements[i] = p->elements[i] * x;
+    }
+    
 } 
