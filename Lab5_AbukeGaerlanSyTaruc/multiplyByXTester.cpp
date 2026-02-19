9310f6333bff9607fc7d10b300d4383f5811a53a
Goodymind <aabuke9@gmail.com>
2026-02-20 03:51:01 +0800

    A.5 multiplyByX.out generated

--- Lab5_AbukeGaerlanSyTaruc/multiplyByXTester.cpp
+++ Lab5_AbukeGaerlanSyTaruc/multiplyByXTester.cpp
@@ -1,6 +1,8 @@
 #include <iostream> 
 #include "IntArray.h" 
 
+using namespace std;
+
 extern void multiplyByX(IntArray *p, int x); 
 
 int main() 
@@ -10,6 +12,23 @@ int main()
     // - print it out 
     // - multiply it by a number of your choosing via multiplyByX() 
     // - print the result 
+
+    IntArray arr;
+    arr.elements = new int[5];
+    arr.elements[0] = 0;
+    arr.elements[1] = 1;
+    arr.elements[2] = 2;
+    arr.elements[3] = 3;
+    arr.elements[4] = 4;
+
+    cout << arr.elements[0] << endl;
+    cout << arr.elements[1] << endl;
+    cout << arr.elements[2] << endl;
+    cout << arr.elements[3] << endl;
+    cout << arr.elements[4] << endl;
+
+    delete arr.elements;
+    arr.elements = NULL;
 
     return 0; 
 } 
