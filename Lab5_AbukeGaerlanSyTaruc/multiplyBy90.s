3178b20bce8e961c1ec8b8037f7983404c5304cb
Goodymind <aabuke9@gmail.com>
2026-02-25 20:20:40 +0800

    multiplyBy90Tester

---
+++ Lab5_AbukeGaerlanSyTaruc/multiplyBy90.s
@@ -0,0 +1,52 @@
+ .file "multiplyB90.cpp"
+ .text
+ .globl _Z12multiplyBy90P8IntArray
+ .type _Z12multiplyBy90P8IntArray, @function
+_Z12multiplyBy90P8IntArray:
+.LFB0:
+ .cfi_startproc
+ endbr64
+ cmpl $0, (%rdi)
+ jle .L1
+ movl $0, %eax
+.L3:
+ movq 8(%rdi), %rdx
+ leaq (%rdx,%rax,4), %rdx
+ movl (%rdx), %ebx
+ movl $0, %ecx
+ shl $1, %ebx
+ addl %ebx, %ecx
+ shl $6, %ebx
+ addl %ebx, %ecx
+ movl (%rdx), %ebx
+ shl $3, %ebx
+ subl %ebx, %ecx
+ shl $2, %ebx
+ subl %ebx, %ecx
+ movl %ecx, (%rdx)
+ addq $1, %rax
+ cmpl %eax, (%rdi)
+ jg .L3
+.L1:
+ ret
+ .cfi_endproc
+LFE0:
+ .size _Z12multiplyBy90P8IntArray, .-_Z12multiplyBy90P8IntArray
+ .ident "GCC: (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0"
+ .section .note.GNU-stack,"",@progbits
+ .section .note.gnu.property,"a"
+ .align 8
+ .long 1f - 0f
+ .long	4f - 1f
+ .long 5
+0:
+ .string "GNU"
+1:
+ .align 8
+ .long 0xc0000002
+ .long 3f - 2f
+2:
+ .long 0x3
+3:
+ .align 8
+4:
---
+++ Lab5_AbukeGaerlanSyTaruc/multiplyBy90Tester.cpp
@@ -0,0 +1,40 @@
+#include <iostream> 
+#include "IntArray.h" 
+
+using namespace std;
+
+extern void multiplyBy90(IntArray *p); 
+ 
+int main() 
+{ 
+    IntArray* arr = new IntArray;
+    arr->size = 5;
+    arr->elements = new int[5];
+    arr->elements[0] = 0;
+    arr->elements[1] = 1;
+    arr->elements[2] = 2;
+    arr->elements[3] = 3;
+    arr->elements[4] = 4;
+
+    cout << arr->elements[0] << endl;
+    cout << arr->elements[1] << endl;
+    cout << arr->elements[2] << endl;
+    cout << arr->elements[3] << endl;
+    cout << arr->elements[4] << endl;
+
+    multiplyBy90(arr);
+
+    cout << arr->elements[0] << endl;
+    cout << arr->elements[1] << endl;
+    cout << arr->elements[2] << endl;
+    cout << arr->elements[3] << endl;
+    cout << arr->elements[4] << endl;
+
+    delete arr->elements;
+    arr->elements = NULL;
+
+    delete arr;
+    arr = NULL;
+
+    return 0; 
+} 
