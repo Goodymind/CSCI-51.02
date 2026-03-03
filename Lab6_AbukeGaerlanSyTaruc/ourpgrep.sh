c4dce73cf80af47b3be086c7de0ba3de0e1b29da
tobi-212 <tobi@tobis-laptop.localdomain>
2026-03-03 09:17:33 +0800

    fixed kinda

--- Lab6_AbukeGaerlanSyTaruc/ourpgrep.sh
+++ Lab6_AbukeGaerlanSyTaruc/ourpgrep.sh
@@ -2,9 +2,10 @@
 input=""
 read input
 current_user=$USER
+char_length=${#current_user}
 
 if [ -n "$input" ]; then
-	ps aux | grep "$current_user" | grep "$input" | grep -v grep
+	ps aux | grep "$current_user" | grep "$input" | grep -v grep | cut -c "$((char_length+10))"-"$((char_length+13))"
 else
 	echo "Please input a program name to search"
 fi
