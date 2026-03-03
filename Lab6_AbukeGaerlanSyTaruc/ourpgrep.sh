355ab0d1004c3e9affaddedf6a0012cba4e27980
tobi-212 <tobi@tobis-laptop.localdomain>
2026-03-03 09:23:28 +0800

    pid length can be longer than 3 so here's a temp fix

--- Lab6_AbukeGaerlanSyTaruc/ourpgrep.sh
+++ Lab6_AbukeGaerlanSyTaruc/ourpgrep.sh
@@ -5,7 +5,7 @@ current_user=$USER
 char_length=${#current_user}
 
 if [ -n "$input" ]; then
-	ps aux | grep "$current_user" | grep "$input" | grep -v grep | cut -c "$((char_length+10))"-"$((char_length+13))"
+	ps aux | grep "$current_user" | grep "$input" | grep -v grep | cut -c "$((char_length+10))"-"$((char_length+14))"
 else
 	echo "Please input a program name to search"
 fi
