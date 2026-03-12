44ccfe9577667804add900257ef7b7fdae4e8c45
Goodymind <aabuke9@gmail.com>
2026-03-12 20:25:13 +0800

    setup

---
+++ Lab7_AbukeGaerlanSyTaruc/mytimer.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+// fork()
+#include <unistd.h>
+
+// date time
+#include <ctime>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    while (true)
+    {
+        
+    }
+    
+    return 0;
+}
+
+// sources
+/*
+    date-time: https://www.w3schools.com/cpp/cpp_date.asp
+
+*/
