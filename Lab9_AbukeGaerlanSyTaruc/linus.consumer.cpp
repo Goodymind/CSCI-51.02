4febefe94eb2dbb513dcad59d9411ff21cceb250
Goodymind <aabuke9@gmail.com>
2026-04-26 21:36:00 +0800

    consumer done

---
+++ Lab9_AbukeGaerlanSyTaruc/linus.consumer.cpp
@@ -0,0 +1,116 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// strings and reading from terminal
+#include <string>
+#include <iostream>
+
+// shared variables
+#include "shared.h"
+
+// listener process
+#include <unistd.h>
+#include <csignal>
+
+// filereading
+// https://cplusplus.com/doc/tutorial/files/
+#include <fstream>
+
+// sleep for ms
+#include <chrono>
+#include <thread>
+
+// strcpy
+#include <cstring>
+
+void readSharedMemory()
+{
+    int shmId;
+    int shmFlags = IPC_CREAT | 0666;
+    char *sharedMem;
+    shmId = shmget(SHM_KEY, MAX_FRAME_SIZE, shmFlags);
+    sharedMem = (char *)shmat(shmId, NULL, 0);
+
+    if (((int *)sharedMem) == (int *)-1)
+    {
+        perror("shmop: shmat failed");
+    }
+
+    else
+    {
+        char buffer[MAX_FRAME_SIZE];
+        strcpy(buffer, sharedMem);
+        std::cout << buffer << std::endl;
+    }
+}
+
+void trySharedMemory()
+{
+    // -- Sempahore get
+    int nSems = 1; // number of processes that can access shared memory at the same time (right?)
+    int semFlag = IPC_CREAT | 0666;
+    int semId = semget(SEM_KEY, nSems, semFlag);
+
+    if (semId == -1)
+    {
+        perror("semget");
+        std::exit(1);
+    }
+    // -- Semaphore access
+    int nOperations = 2;
+    struct sembuf sema[nOperations];
+    sema[0].sem_num = 0;
+    sema[0].sem_op = 0; // wait if semaphore != 0
+    sema[0].sem_flg = SEM_UNDO;
+
+    sema[1].sem_num = 0;
+    sema[1].sem_op = 1; // increment semaphore by 1
+    sema[1].sem_flg = SEM_UNDO | IPC_NOWAIT;
+    int opResult = semop(semId, sema, nOperations);
+    if (opResult != -1)
+    {
+        readSharedMemory();
+
+        // -- Semaphore release
+        nOperations = 1;
+        sema[0].sem_num = 0;
+        sema[0].sem_op = -1; // decrement, return semaphore to 0
+        sema[0].sem_flg = SEM_UNDO | IPC_NOWAIT;
+
+        opResult = semop(semId, sema, nOperations);
+        if (opResult == -1)
+        {
+            perror("semop (decrement)");
+        }
+    }
+}
+int main(int argc, char* argv[])
+{
+    int fps = atoi(argv[1]);
+
+    // #3, listener process
+    pid_t sellerPid = getpid();
+    pid_t listener = fork();
+    if (listener == 0)
+    {
+        std::string res;
+        std::getline(std::cin, res);
+        std::cout << "Listener received input: " << res << std::endl;
+        kill(sellerPid, SIGINT);
+        exit(0);
+    }
+
+    // main loop, as usual
+    int interval = 1000 / fps;
+    while (true)
+    {
+        trySharedMemory();
+        std::this_thread::sleep_for(std::chrono::milliseconds(interval));
+    }
+
+}
